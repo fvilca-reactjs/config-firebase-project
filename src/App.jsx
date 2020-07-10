c904177368577059a6105fb01dc51998a6989559
fvilca1 <fvilca@gmail.com>
2020-07-09 23:20:22 -0500

    try to add sounds effect useSound Hook

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,18 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Login from './components/login';
 import { Firebase2 } from './server/firebase2'
 import { SessionContext } from './session/store';
-
+import useSound from 'use-sound'
+import soundBirds from './sounds/amb_city_birds.mp3'
+import soundBeep from './sounds/ui_a.mp3'
 
 function App() {
 
   const firebase = useContext(Firebase2.Context);  //instanciando un contexto
   const [firebaseIsReady, setFirebaseIsReady] = useState(false)
-
   const [{ openSnackbar }, dispatch] = useContext(SessionContext)
+
+  const [play1] = useSound(soundBeep)
+  const [play2] = useSound(soundBirds)
 
   useEffect(() => {
     firebase.isReady()
@@ -21,35 +25,28 @@ function App() {
         setFirebaseIsReady(true)
       })
       .catch(error => console.log(error, "firebase no responde"))
-
   }, []);
 
-  const handleClose = () => {
-    dispatch({
-      type: 'OPEN_SNACKBAR',
-      mensaje: ''
-    })
-    openSnackbar.open = false;
-    openSnackbar.mensaje = '';
-  }
 
   return (
     firebaseIsReady ?
       <div className="App">
         <div >
           <span>{openSnackbar ? openSnackbar.mensaje : ''}</span>
-          <button onClick={handleClose}></button>
+          x
         </div>
         <Router>
           <header>
             <Link to='/auth/registrar'
-              onMouseEnter={() => console.log('enter:true')}
-              onMouseLeave={() => console.log('leave:true')}>Registrar Usuario</Link>
-            <Link to='/auth/login'>Login</Link>
-            <button
+              onMouseEnter={() => { play1() }}
+            >Registrar Usuario</Link>
+            <Link to='/auth/login'
+              onMouseEnter={() => { play1(); }}
+            >Login</Link>
+            <Link
               to='/'
-              component={Link}>
-              Home</button>
+            >
+              Home</Link>
           </header>
           <Route path='/auth/registrar' exact component={RegistrarUsuario}></Route>
           <Route path='/auth/login' exact component={Login}></Route>
